8681bb9f7be9978c876968ab45310fecca2781ee
anejaagam <aneja.agamjot@gmail.com>
2023-04-02 22:10:48 -0700

    Front end cleanup for HOME

--- skid/lib/screens/home_screen.dart
+++ skid/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:skid/screens/login_screen.dart';
 import 'package:skid/screens/map_view_screen.dart';
 import 'package:skid/services/authentication_service.dart';
 
@@ -21,6 +22,34 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  int _currentIndex = 0;
+  final List<Widget> _pages = [
+    // Add the widgets for your pages here
+    Center(child: Text('Home')),
+    Center(child: Text('Map')),
+    Center(child: Text('Rides')),
+    Center(child: Text('Logout')),
+  ];
+  void _navigateToPage(int index) async {
+    Widget page;
+    switch (index) {
+      case 0:
+        page = HomeScreen();
+        break;
+      case 1:
+        page = MapViewScreen();
+        break;
+      default:
+        await _authService.signOut();
+        page = LoginScreen();
+    }
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => page),
+      (route) => false,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +64,7 @@ class _HomeScreenState extends State<HomeScreen> {
           children: [
             Text(
               'Welcome $_userName',
-              style: Theme.of(context).textTheme.headline4,
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
             SizedBox(height: 24),
             ElevatedButton(
@@ -73,6 +102,40 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+          _navigateToPage(index);
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Map',
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bike_scooter),
+            label: 'Rides',
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.logout),
+            label: 'Logout',
+            backgroundColor: Colors.black,
+          ),
+        ],
+        // Set the background color
+        selectedItemColor: Colors.white, // Set the selected item color
+        unselectedItemColor: Colors.white,
       ),
     );
   }
