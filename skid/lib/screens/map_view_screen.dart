eec81bd283eed60b5c1c71b0492ac343b384e0c2
andydevarakonda <dvsanirudh@gmail.com>
2023-04-13 20:36:24 -0700

    uploaded the live tracker, distance,  naviagation

--- skid/lib/screens/map_view_screen.dart
+++ skid/lib/screens/map_view_screen.dart
@@ -1,26 +1,50 @@
-import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart'; 
-import 'package:google_maps_flutter/google_maps_flutter.dart' as gmaps;
-import 'package:geolocator/geolocator.dart';
-//import 'package:google_maps_webservice/directions.dart' as gmaps_ws;
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:polyline/polyline.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'dart:async';
+import 'dart:collection';
+import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart' as geo;
+import 'package:location/location.dart' as loc;
 
+import 'LocationService.dart';
 
+void main() => runApp(MapViewScreen());
 
-class MapViewScreen extends StatefulWidget {
+class MapViewScreen extends StatelessWidget {
   @override
-  _MapViewScreenState createState() => _MapViewScreenState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Maps and Navigation',
+      home: MapSample(),
+    );
+  }
 }
 
-class _MapViewScreenState extends State<MapViewScreen> {
-  late GoogleMapController _mapController;
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+  TextEditingController _originController = TextEditingController();
+  PolylineId? _currentRoutePolylineId;
   LatLng _initialCameraPosition = const LatLng(0, 0);
+  TextEditingController _destinationController = TextEditingController();
+  loc.Location location = new loc.Location();
+  loc.LocationData? _locationData;
+  Set<Marker> _markers = Set<Marker>();
+  Set<Polygon> _polygons = Set<Polygon>();
+  Set<Polyline> _polylines = Set<Polyline>();
+  List<LatLng> polygonLatLngs = <LatLng>[];
+
+  int _polygonIdCounter = 1;
+  int _polylineIdCounter = 1;
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(49.940605743955274, -119.39517000394746),
+    zoom: 18.0000,
+  );
 
   @override
   void initState() {
@@ -28,36 +52,35 @@ class _MapViewScreenState extends State<MapViewScreen> {
     _getUserLocation();
   }
 
-  Future<List<LatLng>> _fetchRouteData(LatLng origin, LatLng destination) async {
-  const apiKey = 'AIzaSyDsrdxHDGObMKB9WcdkxeZxaft2t0DEgkw';
-  final url = Uri.parse(
-      'https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}&destination=${destination.latitude},${destination.longitude}&mode=driving&key=$apiKey');
-
-  final response = await http.get(url);
-
-  if (response.statusCode == 200) {
-    final jsonResponse = jsonDecode(response.body);
-
-    if (jsonResponse['status'] == 'OK') {
-      final points = jsonResponse['routes'][0]['overview_polyline']['points'];
-      final PolylinePoints polylinePoints = PolylinePoints();
-      final List<PointLatLng> decodedPoints = polylinePoints.decodePolyline(points);
-      final latLngPoints = decodedPoints.map((e) => LatLng(e.latitude, e.longitude)).toList();
-
-      return latLngPoints;
-    } else {
-      throw Exception('Failed to fetch directions: ${jsonResponse['status']}');
-    }
-  } else {
-    throw Exception('Failed to fetch directions with status code: ${response.statusCode}');
+  void _setMarker(LatLng point) {
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: MarkerId('marker'),
+          position: point,
+        ),
+      );
+    });
   }
-}
 
+  void _setPolygon() {
+    final String polygonIdVal = 'polygon_$_polygonIdCounter';
+    _polygonIdCounter++;
 
+    _polygons.add(
+      Polygon(
+        polygonId: PolygonId(polygonIdVal),
+        points: polygonLatLngs,
+        strokeWidth: 0,
+        fillColor: Colors.transparent,
+      ),
+    );
+  }
 
   Future<void> _getUserLocation() async {
-    LocationPermission permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.deniedForever) {
+    geo.LocationPermission permission =
+        await geo.Geolocator.requestPermission();
+    if (permission == geo.LocationPermission.deniedForever) {
       // ignore: use_build_context_synchronously
       return showDialog(
         context: context,
@@ -75,123 +98,189 @@ class _MapViewScreenState extends State<MapViewScreen> {
         },
       );
     }
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
+    geo.Position position = await geo.Geolocator.getCurrentPosition(
+        desiredAccuracy: geo.LocationAccuracy.high);
     print('User position: $position'); // Add this line
     setState(() {
       _initialCameraPosition = LatLng(position.latitude, position.longitude);
+      _originController.text = "${position.latitude},${position.longitude}";
     });
   }
 
-  void _onMapCreated(GoogleMapController controller) {
-    _mapController = controller;
-    setState(() {});
-  }
-
-  Set<gmaps.Polyline> _polylines = {};
-
-void _addPolyline(List<LatLng> points) {
-  final polylineId = gmaps.PolylineId('${DateTime.now().millisecondsSinceEpoch}');
-  final polyline = gmaps.Polyline(
-    polylineId: polylineId,
-    points: points,
-    color: Colors.red,
-    width: 5,
-  );
-  setState(() {
-    _polylines.add(polyline);
-  });
-}
-
-
-  StreamSubscription<Position>? _positionStream;
-  List<Position> _positions = [];
-  DateTime _rideStartTime = DateTime.now();
-
-  void _startRide() {
-  _rideStartTime = DateTime.now();
-  final locationSettings = const LocationSettings(
-    accuracy: LocationAccuracy.high,
-    distanceFilter: 10,
-  );
-  _positionStream = Geolocator.getPositionStream(locationSettings: locationSettings).listen((Position position) {
-    setState(() {
-      _positions.add(position);
-    });
-  });
-}
-
-
-  void _endRide() {
-    _positionStream?.cancel();
-    final rideDuration = DateTime.now().difference(_rideStartTime);
-    double totalDistance = 0;
-    for (int i = 0; i < _positions.length - 1; i++) {
-      totalDistance += Geolocator.distanceBetween(
-        _positions[i].latitude,
-        _positions[i].longitude,
-        _positions[i + 1].latitude,
-        _positions[i + 1].longitude,
+  void _updatePolyline() async {
+    if (_destinationController.text.isNotEmpty) {
+      // Calculate the new route based on the updated user location
+      List<PointLatLng> points = await LocationService.getRouteCoordinates(
+        _originController.text,
+        _destinationController.text,
       );
+
+      // Remove the old route if it exists
+      if (_currentRoutePolylineId != null) {
+        setState(() {
+          _polylines.removeWhere((Polyline polyline) =>
+              polyline.polylineId == _currentRoutePolylineId);
+        });
+      }
+
+      final String polylineIdVal = 'polyline_$_polylineIdCounter';
+      _polylineIdCounter++;
+
+      Polyline newRoute = Polyline(
+        polylineId: PolylineId(polylineIdVal),
+        width: 6,
+        color: Color.fromARGB(255, 255, 0, 0),
+        points: points
+            .map(
+              (point) => LatLng(point.latitude, point.longitude),
+            )
+            .toList(),
+      );
+
+      setState(() {
+        _polylines.add(newRoute);
+        _currentRoutePolylineId = newRoute.polylineId;
+      });
     }
-    final averageSpeed = totalDistance / rideDuration.inSeconds;
-print('Ride duration: $rideDuration');
-print('Average speed: $averageSpeed m/s');
+  }
 
-// Save the ride data to Firestore
-_saveRideData(
-  route: _positions.map((position) => LatLng(position.latitude, position.longitude)).toList(),
-  rideDuration: rideDuration,
-  averageSpeed: averageSpeed,
-);
-}
+  void _setupLocationListener() {
+    location.enableBackgroundMode(enable: true);
+    location.onLocationChanged.listen((loc.LocationData currentLocation) {
+      setState(() {
+        _locationData = currentLocation;
+        _originController.text =
+            "\${currentLocation.latitude},\${currentLocation.longitude}";
+        _updatePolyline();
+      });
+    });
+  }
 
-Future<void> _saveRideData({
-required List<LatLng> route,
-required Duration rideDuration,
-required double averageSpeed,
-}) async {
-final user = FirebaseAuth.instance.currentUser;
-if (user == null) {
-  throw Exception('User not signed in.');
-}
-
-final routeData = route.map((point) => {'lat': point.latitude, 'lng': point.longitude}).toList();
-final rideData = {
-  'route': routeData,
-  'rideDuration': rideDuration.inSeconds,
-  'averageSpeed': averageSpeed,
-  'timestamp': FieldValue.serverTimestamp(),
-};
-
-await FirebaseFirestore.instance
-    .collection('users')
-    .doc(user.uid)
-    .collection('rides')
-    .add(rideData);
-}
-
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text('Map View'),
-    ),
-    body: SizedBox(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height * 0.9,
-      child: GoogleMap(
-        initialCameraPosition: CameraPosition(target: _initialCameraPosition, zoom: 15),
-        myLocationEnabled: true,
-        myLocationButtonEnabled: true,
-        onMapCreated: _onMapCreated,
-        polylines: _polylines,
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('Maps and Navigation '),
+        backgroundColor: Color.fromARGB(255, 255, 52, 2),
       ),
-    ),
-  );
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: _originController,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            hintText: '  Enter Start Address',
+                            icon: Icon(Icons.gps_fixed)),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: _destinationController,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            hintText: '  Enter Destination Address',
+                            helperText:
+                                'Ex: 1234 International Mews, Kelowna, BC V1Y 9X3',
+                            suffixIcon: Icon(Icons.location_on_outlined)),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: GoogleMap(
+              mapType: MapType.satellite,
+              myLocationEnabled: true,
+              markers: _markers,
+              polygons: _polygons,
+              polylines: _polylines,
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+              onTap: (point) {
+                setState(
+                  () {
+                    polygonLatLngs.add(point);
+                    _setPolygon();
+                  },
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FloatingActionButton.extended(
+              extendedPadding: const EdgeInsets.all(8.0),
+              label: const Text('Calculate Distance'),
+              icon: const Icon(Icons.route),
+              backgroundColor: Colors.deepPurple.shade900,
+              onPressed: () async {
+                var directions = await LocationService.getDirections(
+                  _originController.text,
+                  _destinationController.text,
+                );
+                _goToPlace(
+                  directions['start_location']['lat'],
+                  directions['start_location']['lng'],
+                  directions['bounds_ne'],
+                  directions['bounds_sw'],
+                );
+                _updatePolyline();
+              },
+            ),
+          ),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: RichText(
+                text: TextSpan(
+                  text: 'Version 1.0',
+                  style: TextStyle(color: Colors.black, fontSize: 12),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _goToPlace(
+    double lat,
+    double lng,
+    Map<String, dynamic> boundsNe,
+    Map<String, dynamic> boundsSw,
+  ) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(target: LatLng(lat, lng), zoom: 12),
+      ),
+    );
+    controller.animateCamera(
+      CameraUpdate.newLatLngBounds(
+          LatLngBounds(
+            southwest: LatLng(boundsSw['lat'], boundsSw['lng']),
+            northeast: LatLng(boundsNe['lat'], boundsNe['lng']),
+          ),
+          25),
+    );
+    _setMarker(LatLng(lat, lng));
+  }
 }
-
-}
-
-
-
--- skid/macos/Flutter/GeneratedPluginRegistrant.swift
+++ skid/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,8 @@ import firebase_core
 import firebase_database
 import geolocator_apple
 import location
+import package_info_plus
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -19,4 +21,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
