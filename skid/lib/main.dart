0038a39efde2acf3f0a7da92684d92f3bdaa3fb8
isaacxj <isaacjoseph2001@gmail.com>
2023-04-14 00:57:19 -0700

    bt wrong

--- skid/lib/main.dart
+++ skid/lib/main.dart
@@ -5,22 +5,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:skid/screens/home_screen.dart';
 import 'package:skid/screens/login_screen.dart';
 import 'package:skid/screens/signup_screen.dart';
+import 'package:provider/provider.dart';
 
-// void main() {
-//  WidgetsFlutterBinding.ensureInitialized();
-//  Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-// );
-//   runApp( MyApp());
-// }
 
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp().catchError((error) {
-//     print('Error initializing Firebase: $error');
-//   });
-//   runApp(MyApp());
-// }
+
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,31 +22,39 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SKID App',
-      theme: ThemeData(
-        useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      routes: {
-        '/login': (context) => LoginScreen(),
-        '/signup': (context) => SignupScreen(),
-        '/home': (context) => HomeScreen(),
-      },
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else {
-            if (snapshot.hasData) {
-              return HomeScreen();
-            } else {
-              return LoginScreen();
-            }
-          }
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<BluetoothConnection>(
+          create: (context) => BluetoothConnection(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'SKID App',
+        theme: ThemeData(
+          useMaterial3: true,
+          primarySwatch: Colors.blue,
+        ),
+        routes: {
+          '/login': (context) => LoginScreen(),
+          '/signup': (context) => SignupScreen(),
+          '/home': (context) => HomeScreen(),
         },
+        home: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else {
+              if (snapshot.hasData) {
+                return HomeScreen();
+              } else {
+                return LoginScreen();
+              }
+            }
+          },
+        ),
       ),
     );
   }
 }
+
--- skid/lib/screens/home_screen.dart
+++ skid/lib/screens/home_screen.dart
@@ -1,8 +1,135 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:skid/screens/login_screen.dart';
 import 'package:skid/screens/map_view_screen.dart';
 import 'package:skid/services/authentication_service.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:provider/provider.dart';
+import 'package:permission_handler/permission_handler.dart' as ph;
+import 'package:location/location.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => BluetoothConnection(),
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+
+class BluetoothConnection extends ChangeNotifier {
+  final FlutterReactiveBle _ble = FlutterReactiveBle();
+  final Uuid _targetService = Uuid.parse("00001101-0000-1000-8000-00805F9B34FB");
+  StreamSubscription<DiscoveredDevice>? _scanSubscription;
+  StreamSubscription<ConnectionStateUpdate>? _connectionSubscription;
+  ConnectionStateUpdate? _connectionState;
+  bool _isScanning = false;
+  late StreamSubscription<ConnectionStateUpdate> _connection;
+
+  Future<void> checkLocationServicesAndPermission() async {
+  Location location = Location();
+  bool serviceEnabled;
+  ph.PermissionStatus permissionStatus;
+
+  // Check if location services are enabled
+  serviceEnabled = await location.serviceEnabled();
+  if (!serviceEnabled) {
+    serviceEnabled = await location.requestService();
+    if (!serviceEnabled) {
+      // Show an error message or prompt the user to enable location services
+      return;
+    }
+  }
+
+  // Request location permission
+  permissionStatus = await ph.Permission.location.request();
+  if (permissionStatus.isGranted) {
+    // Start scanning for devices
+  } else {
+    // Show an error message or prompt the user to grant permission
+  }
+}
+
+
+  Future<void> startScan() async {
+    if (_isScanning) return;
+    await checkLocationServicesAndPermission();
+    _isScanning = true;
+    _scanSubscription = _ble.scanForDevices(
+      withServices: [_targetService],
+      scanMode: ScanMode.lowLatency,
+    ).listen((device) {
+      if (device.name == "HC-05") {
+        stopScan();
+        _connectToDevice(device.id);
+      }
+    });
+  }
+
+  void stopScan() {
+    _scanSubscription?.cancel();
+    _scanSubscription = null;
+    _isScanning = false;
+  }
+
+  Future<void> _connectToDevice(String deviceId) async {
+    _connectionSubscription?.cancel();
+    _connectionSubscription = _ble.connectToAdvertisingDevice(
+      id: deviceId,
+      withServices: [_targetService],
+      prescanDuration: const Duration(seconds: 5),
+      connectionTimeout: const Duration(seconds: 2),
+    ).listen((connectionState) {
+      _connectionState = connectionState;
+      if (connectionState.connectionState == DeviceConnectionState.connected) {
+        print('Connected to HC-05');
+      } else if (connectionState.connectionState ==
+          DeviceConnectionState.disconnected) {
+        print('Disconnected from HC-05');
+      }
+    });
+
+  }
+
+        Future<void> disconnect() async {
+    if (_connectionState == null ||
+        _connectionState!.connectionState != DeviceConnectionState.connected) {
+      return;
+    }
+    try {
+      await _connection.cancel();
+    } catch (e) {
+      print("Error disconnecting from device: $e");
+    }
+  }
+
+
+
+
+  @override
+  void dispose() {
+    _scanSubscription?.cancel();
+    _connectionSubscription?.cancel();
+    super.dispose();
+  }
+}
+
+
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -13,6 +140,7 @@ class _HomeScreenState extends State<HomeScreen> {
   User? _user;
   String _userName = '';
   final _authService = AuthenticationService();
+
   @override
   void initState() {
     super.initState();
@@ -20,34 +148,6 @@ class _HomeScreenState extends State<HomeScreen> {
     if (_user != null) {
       _userName = _user!.displayName ?? '';
     }
-  }
-
-  int _currentIndex = 0;
-  final List<Widget> _pages = [
-    // Add the widgets for your pages here
-    Center(child: Text('Home')),
-    Center(child: Text('Map')),
-    Center(child: Text('Rides')),
-    Center(child: Text('Logout')),
-  ];
-  void _navigateToPage(int index) async {
-    Widget page;
-    switch (index) {
-      case 0:
-        page = HomeScreen();
-        break;
-      case 1:
-        page = MapViewScreen();
-        break;
-      default:
-        await _authService.signOut();
-        page = LoginScreen();
-    }
-    Navigator.pushAndRemoveUntil(
-      context,
-      MaterialPageRoute(builder: (context) => page),
-      (route) => false,
-    );
   }
 
   @override
@@ -62,81 +162,50 @@ class _HomeScreenState extends State<HomeScreen> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text(
-              'Welcome $_userName',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () {
-                // Implement Bluetooth connection
-              },
-              child: Text('Connect to Bike'),
-            ),
-            SizedBox(height: 12),
-            ElevatedButton(
-              onPressed: () {
-                // Implement navigation to the map screen
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MapViewScreen()),
-                );
-              },
-              child: Text('View Map'),
-            ),
-            SizedBox(height: 12),
-            ElevatedButton(
-              onPressed: () {
-                // Implement navigation to the past rides screen
-              },
-              child: Text('View Past Rides'),
-            ),
-            SizedBox(height: 12),
-            ElevatedButton(
-              onPressed: () async {
-                // Implement navigation to the past rides screen
-                await _authService.signOut();
-                Navigator.pushReplacementNamed(context, '/login');
-              },
-              child: Text('Logout'),
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-          _navigateToPage(index);
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-            backgroundColor: Colors.black,
+            Text('Welcome $_userName',
+            style: Theme.of(context).textTheme.headlineMedium,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.map),
-            label: 'Map',
-            backgroundColor: Colors.black,
+          SizedBox(height: 24),
+          ElevatedButton(
+  onPressed: () {
+    final connection =
+        Provider.of<BluetoothConnection>(context, listen: false);
+    connection.startScan();
+  },
+  child: Text('Connect to Bike'),
+),
+
+
+          SizedBox(height: 12),
+          ElevatedButton(
+            onPressed: () {
+              // Implement navigation to the map screen
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MapViewScreen()),
+              );
+            },
+            child: Text('View Map'),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.bike_scooter),
-            label: 'Rides',
-            backgroundColor: Colors.black,
+          SizedBox(height: 12),
+          ElevatedButton(
+            onPressed: () {
+              // Implement navigation to the past rides screen
+            },
+            child: Text('View Past Rides'),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.logout),
-            label: 'Logout',
-            backgroundColor: Colors.black,
+          SizedBox(height: 12),
+          ElevatedButton(
+            onPressed: () async {
+              // Implement navigation to the past rides screen
+              await _authService.signOut();
+              Navigator.pushReplacementNamed(context, '/login');
+            },
+            child: Text('Logout'),
           ),
         ],
-        // Set the background color
-        selectedItemColor: Colors.white, // Set the selected item color
-        unselectedItemColor: Colors.white,
       ),
-    );
+    ),);
   }
 }
+
